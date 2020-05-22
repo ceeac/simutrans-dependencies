4a45d7b186b9fa0d0e109d46b7b4499d259443e0
ceeac <ceeac@users.noreply.github.com>
2020-05-22 14:00:05 +0200

    Add sdl2-mixer

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 setlocal
 pushd vcpkg
-  set libraries=zlib:%TRIPLET% bzip2:%TRIPLET% zstd:%TRIPLET% libpng:%TRIPLET% freetype:%TRIPLET% sdl2:%TRIPLET% miniupnpc:%TRIPLET%
+  set libraries=zlib:%TRIPLET% bzip2:%TRIPLET% zstd:%TRIPLET% libpng:%TRIPLET% freetype:%TRIPLET% sdl2:%TRIPLET% sdl2-mixer:%TRIPLET% miniupnpc:%TRIPLET%
   .\vcpkg install %libraries%
   .\vcpkg export %libraries% --zip --nuget
 popd
