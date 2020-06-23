790d3965e31afe72bc1714deaef4d84d926d16fd
ceeac <ceeac@users.noreply.github.com>
2020-06-23 19:20:21 +0200

    Add GLEW

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 setlocal
 pushd vcpkg
-  set libraries=zlib:%TRIPLET% bzip2:%TRIPLET% zstd:%TRIPLET% libpng:%TRIPLET% freetype:%TRIPLET% sdl1:%TRIPLET% sdl2:%TRIPLET% sdl2-mixer:%TRIPLET% miniupnpc:%TRIPLET% pthreads:%TRIPLET%
+  set libraries=zlib:%TRIPLET% bzip2:%TRIPLET% zstd:%TRIPLET% libpng:%TRIPLET% freetype:%TRIPLET% glew:%TRIPLET% sdl1:%TRIPLET% sdl2:%TRIPLET% sdl2-mixer:%TRIPLET% miniupnpc:%TRIPLET% pthreads:%TRIPLET%
   .\vcpkg install %libraries%
   .\vcpkg export %libraries% --zip --nuget
 popd
